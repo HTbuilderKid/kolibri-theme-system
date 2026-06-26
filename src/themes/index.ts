d6b5158db474e3d4044fa954c9cfcf028bcfc4c2
HTcodingTH <chenyuw2019@gmail.com>
2026-06-26 11:24:43 -0400

    Add oceanDepthTheme to themes export

--- src/themes/index.ts
+++ src/themes/index.ts
@@ -1,13 +1,16 @@
 import type { ThemeDefinition } from '../core/themeTypes';
 import { forestCanopyTheme } from './forestCanopy.theme';
 import { kolibriDefaultTheme } from './kolibriDefault.theme';
+import { oceanDepthTheme } from './oceanDepth.theme';
 
 export { forestCanopyTheme } from './forestCanopy.theme';
 export { kolibriDefaultTheme } from './kolibriDefault.theme';
+export { oceanDepthTheme } from './oceanDepth.theme';
 
 export const themes: ThemeDefinition[] = [
   kolibriDefaultTheme,
   forestCanopyTheme,
+  oceanDepthTheme,
 ];
 
 export const themeIds = themes.map(theme => theme.metadata.id);
